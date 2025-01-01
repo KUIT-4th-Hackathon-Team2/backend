6602c386faf1ccc65b5704744a0b1732cd12884b
Shinjongyun <tigerjong123@gmail.com>
2025-01-02 03:17:43 +0900

    feat: User 객체 구현

--- src/main/java/com/konkukrent/demo/entity/User.java
+++ src/main/java/com/konkukrent/demo/entity/User.java
@@ -1,4 +1,37 @@
 package com.konkukrent.demo.entity;
 
+import com.konkukrent.demo.entity.enums.Role;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import lombok.*;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 10)
+    private String name;
+
+    @Column(nullable = false)
+    @Email
+    private String email;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    @Builder
+    private User(String name, String email, Role role,
+                 String socialId) {
+        this.name = name;
+        this.email = email;
+        this.role = role;
+    }
+
 }
