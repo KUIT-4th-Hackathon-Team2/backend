6bc66baadc6f80c078a512c5887556ccd0a23231
Shinjongyun <tigerjong123@gmail.com>
2025-01-02 03:57:36 +0900

    feat: UserRequest 구현

--- src/main/java/com/konkukrent/demo/controller/UserController.java
+++ src/main/java/com/konkukrent/demo/controller/UserController.java
@@ -1,4 +1,22 @@
 package com.konkukrent.demo.controller;
 
+import com.konkukrent.demo.entity.User;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
 public class UserController {
+    @PostMapping("/login")
+    public String LoginV4(@ModelAttribute User loggedInUser,
+                          HttpServletRequest request) {
+        log.info("LoginV4");
+        User user = userRepository.findByUserId(loggedInUser.getUserId());
+        if (user != null && user.isSameUser(loggedInUser)) {
+            HttpSession session = request.getSession();
+            session.setAttribute(USER_SESSION_KEY, loggedInUser);
+            return "redirect:/";
+        }
+        return "redirect:/user/loginFailed";
+    }
 }
--- src/main/java/com/konkukrent/demo/dto/UserRequestDto.java
+++ src/main/java/com/konkukrent/demo/dto/UserRequestDto.java
@@ -1,4 +1,12 @@
 package com.konkukrent.demo.dto;
 
+import lombok.Getter;
+
+@Getter
 public class UserRequestDto {
+
+        private String userName;
+        private int studentNum;
+        private String password;
+        private String role;
 }
