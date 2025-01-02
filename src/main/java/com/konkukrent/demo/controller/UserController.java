233e5a62ff78ea079bbbb19d3653348240969a26
Shinjongyun <tigerjong123@gmail.com>
2025-01-02 15:23:24 +0900

    feat: UserController login이랑 signup 구현

--- src/main/java/com/konkukrent/demo/controller/UserController.java
+++ src/main/java/com/konkukrent/demo/controller/UserController.java
@@ -1,22 +1,43 @@
 package com.konkukrent.demo.controller;
 
 import com.konkukrent.demo.entity.User;
+import com.konkukrent.demo.service.UserService;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Tag(name = "Users")
+@Slf4j
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/users")
 public class UserController {
+
+    private final UserService userService;
+    private final SessionManager sessionManager;
+
+
+    @PostMapping("/signup")
+    public ResponseEntity<SignupResponse> signup(@ModelAttribute User newUser) {
+        SignupResponse response = userService.createUser();
+        return ResponseEntity.ok(response);
+    }
+
     @PostMapping("/login")
-    public String LoginV4(@ModelAttribute User loggedInUser,
-                          HttpServletRequest request) {
-        log.info("LoginV4");
-        User user = userRepository.findByUserId(loggedInUser.getUserId());
-        if (user != null && user.isSameUser(loggedInUser)) {
-            HttpSession session = request.getSession();
-            session.setAttribute(USER_SESSION_KEY, loggedInUser);
-            return "redirect:/";
-        }
-        return "redirect:/user/loginFailed";
+    public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest loginRequest) {
+        LoginResponse response = userService.authenticateUser(loginRequest);
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<LoginResponse> logout(@CookieValue(name = "sessionId", required = false) String sessionId) {
     }
 }
--- src/main/java/com/konkukrent/demo/service/UserService.java
+++ src/main/java/com/konkukrent/demo/service/UserService.java
@@ -1,4 +1,68 @@
 package com.konkukrent.demo.service;
 
+import java.util.Optional;
+import
+import com.konkukrent.demo.entity.User;
+import com.konkukrent.demo.entity.enums.Role;
+import com.konkukrent.demo.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
 public class UserService {
+
+    private final UserRepository userRepository;
+
+    public User authenticateUser(LoginRequestDTO loginRequestDTO) {
+        User user = userRepository.findByUserId(loginRequestDTO.getUserId());
+
+        if (user != null && user.getPassword().equals(loginRequestDTO.getPassword())) {
+            return user;
+        }
+        return null;
+    }
+
+    public User registerUser(String name, String email, String socialId, SocialType socialType,
+                             Role role) {
+        User user = User.builder()
+                .role(role)
+                .name(name)
+                .build();
+        return userRepository.save(user);
+    }
+
+    public User findByEmail(String email) {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new UserException(ErrorCode.USER_NOT_FOUND));
+    }
+
+    public UserInfoResponse getUserInfo(User user) {
+        return UserInfoResponse.from(user);
+    }
+
+    public void withdraw(Long userId, Optional<String> accessToken, Optional<String> refreshToken) {
+        User user = findUserById(userId);
+        user.delete();
+
+        String access = accessToken
+                .orElseThrow(() -> new UserException(ErrorCode.SECURITY_INVALID_ACCESS_TOKEN));
+        String refresh = refreshToken
+                .orElseThrow(() -> new UserException(ErrorCode.REFRESH_TOKEN_REQUIRED));
+
+        jwtService.isTokenValid(refresh);
+        jwtService.isTokenValid(access);
+        jwtService.deleteRefreshToken(refresh);
+        jwtService.invalidAccessToken(access);
+    }
+
+    public User findUserById(Long id) {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new UserException(ErrorCode.USER_NOT_FOUND));
+        return user;
+    }
+
+    
 }
