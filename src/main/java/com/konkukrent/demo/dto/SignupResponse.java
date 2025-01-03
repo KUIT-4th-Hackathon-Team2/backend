78875b296166eec03a80cb012a64527a4b0e1bc7
Shinjongyun <tigerjong123@gmail.com>
2025-01-03 23:04:20 +0900

    feat: 회원가입 중복 로직 추가

--- src/main/java/com/konkukrent/demo/dto/SignupResponse.java
+++ src/main/java/com/konkukrent/demo/dto/SignupResponse.java
@@ -1,5 +1,6 @@
 package com.konkukrent.demo.dto;
 
+import com.konkukrent.demo.entity.User;
 import lombok.*;
 
 @Getter
@@ -12,4 +13,13 @@ public class SignupResponse{
     private String userName;
     private Long studentNum;
     private String role;
+
+    public static SignupResponse from(User user) {
+        return SignupResponse.builder()
+                .userId(user.getId())
+                .userName(user.getName())
+                .studentNum(user.getStudentNum())
+                .role(user.getRole().name())
+                .build();
+    }
 }
